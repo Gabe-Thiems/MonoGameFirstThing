ae4aa2b0f0871fb9346401179c263c7e41441954
GabeTimesCode <gmwthiems@gmail.com>
2026-02-14 17:47:17 -0600

    big better (Combined PhysicsGameObject and SquareCollider into one Class, Big changes to physics, fully works)

--- Game1.cs
+++ Game1.cs
@@ -14,6 +14,9 @@ public class Game1 : Game
     public const float playerScale = 2f;
     public const int pixelScale = 64;
 
+    public int userGraphicsHeight = 1080;
+    public int userGraphicsWidth = 1920;
+
     Texture2D computerTexture;
     Texture2D recTexture;
     Vector2 playerPosition = new Vector2();
@@ -22,7 +25,7 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
 
     public PhysicsGameObject playerObj;
-    public SquareCollider playerCollider;
+    public PhysicsGameObject squareObj;
 
     public class PhysicsGameObject
     {
@@ -31,53 +34,63 @@ public class Game1 : Game
         public int weight;
         public bool isForceAffected;
         public Vector2 velocity;
-        public SquareCollider collider;
+        //ashdhas
+        public float collider_width;
+        public float collider_height;
+        public bool collider_isSolid;
+        public string tag;
 
-        public PhysicsGameObject(Texture2D texture, Vector2 position, int weight, bool isForceAffected, Vector2 velocity, SquareCollider collider)
+        public Vector2 topLeftCorner
+        {
+            get
+            {
+                return this.position;
+            }
+            set
+            {
+                this.position = value;
+            }
+
+        }
+
+
+        public Vector2 bottomRightCorner
+        {
+            get
+            {
+                return this.position + new Vector2(collider_width, collider_height);
+            }
+        }
+
+        public PhysicsGameObject(Texture2D texture, Vector2 position, int weight, bool isForceAffected, Vector2 velocity, float collider_width, float collider_height, bool collider_isSolid, string tag )
         {
             this.texture = texture;
             this.position = position;
             this.weight = weight;
             this.isForceAffected = isForceAffected;
             this.velocity = velocity;
-            this.collider = collider;
+            this.collider_width = collider_width;
+            this.collider_height = collider_height;
+            this.collider_isSolid = collider_isSolid; 
+            this.tag = tag;
         }
 
         public void ApplyForce(Vector2 direction, float intensity)
         {
             velocity = direction * intensity;
         }
-    }
-
-    public class SquareCollider
-    {
-        public Vector2 topLeftCorner;
-        public Vector2 bottomRightCorner;
-        public int width;
-        public int height;
-        public bool isSolid;
-        public string tag;
-
-        public SquareCollider(Vector2 topLeftCorner, float height, float width, bool isSolid, string tag)
-        {
-            height *= -1; //Flip it for ease
-            this.topLeftCorner = topLeftCorner;
-            this.bottomRightCorner = topLeftCorner + new Vector2(width, height);
-            this.isSolid = isSolid;
-            this.tag = tag;
-        }
 
         public PhysicsGameObject checkForCollisions(List<PhysicsGameObject> colliders)
         {
             foreach (PhysicsGameObject gameObject in colliders)
             {
-                if (gameObject.collider.tag == tag) {continue;} //Stop hitting yourself (collisions with self ignored)
+                if (gameObject.tag == tag) {continue;} //Stop hitting yourself (collisions with self ignored)
 
                 //AHhhhhhhhhhhhh
-                if (!(bottomRightCorner.X < gameObject.collider.topLeftCorner.X || // You are left of other
-                    topLeftCorner.X > gameObject.collider.bottomRightCorner.X || // You are right of other
-                    bottomRightCorner.Y < gameObject.collider.topLeftCorner.Y || // You are below
-                    topLeftCorner.Y > gameObject.collider.bottomRightCorner.Y))   // You are above
+                if (!(bottomRightCorner.X < gameObject.topLeftCorner.X || // You are left of other
+                    topLeftCorner.X > gameObject.bottomRightCorner.X || // You are right of other
+                    bottomRightCorner.Y < gameObject.topLeftCorner.Y || // You are below
+                    topLeftCorner.Y > gameObject.bottomRightCorner.Y))   // You are above
                 {
                     return gameObject;
                 }
@@ -85,9 +98,9 @@ public class Game1 : Game
             }
             return null;
         }
-
-
     }
+
+    
     
     public void Physics(List<PhysicsGameObject> objects)
     {
@@ -106,16 +119,13 @@ public class Game1 : Game
                 {
                     gameObject.velocity = Vector2.Zero;
                 }
-
             }
-
         }
-
-
     }
 
     public Game1()
     {
+
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
@@ -124,6 +134,11 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        _graphics.PreferredBackBufferWidth = userGraphicsWidth;
+        _graphics.PreferredBackBufferHeight = userGraphicsHeight;
+        //_graphics.IsFullScreen = true;
+
+        _graphics.ApplyChanges();
 
         base.Initialize();
     }
@@ -136,21 +151,31 @@ public class Game1 : Game
         computerTexture = Content.Load<Texture2D>("ComputerBoy-1");
         recTexture = Content.Load<Texture2D>("Rectangle");
 
-        playerCollider = new SquareCollider(
-            playerPosition,
-            (pixelScale * playerScale),
-            (pixelScale * playerScale),
-            true,
-            "player"  );
-
         playerObj = new PhysicsGameObject(
             computerTexture, 
             playerPosition, 
             50, 
             true, 
             Vector2.Zero,
-            playerCollider);
-
+            (pixelScale * playerScale),
+            (pixelScale * playerScale),
+            true,
+            "player"
+            );
+        
+        squareObj = new PhysicsGameObject(
+            recTexture,
+            new Vector2(0, 400),
+            0,
+            false,
+            Vector2.Zero,
+            (pixelScale * 64),
+            (pixelScale * 32),
+            true,
+            "rectangle");
+        
+        
+        
         sceneObjects.Add(playerObj);
     }
 
